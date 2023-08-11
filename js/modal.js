cdbba94bd6d99a37942bfadc544ba3f597040bc2
demouserlidia88 <lidia.molodiuc@gmail.com>
2023-08-11 19:25:26 +0300

    Un alt commit 2

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,20 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+	const refs = {
+		openModalBtn: document.querySelector("[data-modal-open]"),
+		closeModalBtn: document.querySelector("[data-modal-close]"),
+		modal: document.querySelector("[data-modal]"),
+		overlay: document.getElementById("overlay"),
+	};
+
+	refs.openModalBtn.addEventListener("click", openModal);
+	refs.closeModalBtn.addEventListener("click", closeModal);
+
+	function openModal() {
+		refs.modal.classList.add("active");
+		refs.overlay.classList.add("active");
+	}
+	function closeModal() {
+		refs.modal.classList.remove("active");
+		refs.overlay.classList.remove("active");
+	}
+})();
